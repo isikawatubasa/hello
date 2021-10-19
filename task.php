ca55564764e9471e9ea2d0c67977de4e8ced8751
isikawa <buffaloisikawa@gmail.com>
2021-10-19 22:09:35 +0900

    add Q1-Q5

--- task.php
+++ task.php
@@ -6,7 +6,8 @@ print("#####q1#####".PHP_EOL);
 $names = ["田中", "佐藤", "佐々木", "高橋"];
 
 // 以下に回答を記載
-
+$names[] = "斎藤";
+print_r($names);
 echo PHP_EOL;
 
 print("#####q2#####".PHP_EOL);
@@ -14,21 +15,28 @@ $array1 = ["dog", "cat", "fish"];
 $array2 = ["bird", "bat", "tiger"];
 
 // 以下に回答を記載
-
+print_r(array_merge($array1,$array2));
 echo PHP_EOL;
 
 print("#####q3#####".PHP_EOL);
 $numbers = [1, 5, 8, 10, 2, 3, 2, 3, 1, 4, 5, 9];
 
 // 以下に回答を記載
-
+$count = 0;    //答えにたどり着けなかった。※復習必要
+foreach($numbers as $number) {
+    if($number === 3) {
+        $count++;
+    }
+}
+print_r($count."回");
 echo PHP_EOL;
 
 print("#####q4#####".PHP_EOL);
 $sports = ["サッカー", "フットサル", null, "野球", "バスケ", null, "バレー"];
 
 // 以下に回答を記載
-
+$sports = array_diff( $sports,[null]);    //答えにたどり着けなかった。※復習必要
+print_r($sports);
 echo PHP_EOL;
 
 print("#####q5#####".PHP_EOL);
@@ -36,7 +44,11 @@ $array1 = [];
 $array2 = [1, 5, 8, 10];
 
 // 以下に回答を記載
+$array1 =[];    //答えにたどり着けなかった。※復習必要
+var_export(empty($array1));
 
+$array2 =[];
+var_export(empty($array2));
 echo PHP_EOL;
 
 print("#####q6#####".PHP_EOL);
